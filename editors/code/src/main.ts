9217e86f2772b61a66561aa53e6c0b71e640bf2c
Victor Pogor <victor.pogor@outlook.com>
2024-12-16 10:39:43 +0000

    feat(vscode): added basic syntax highlighting

--- editors/code/src/main.ts
+++ editors/code/src/main.ts
@@ -1,36 +1,36 @@
 
-import * as path from 'path';
+// import * as path from 'path';
 import { workspace, ExtensionContext } from 'vscode';
 
 import {
     LanguageClient,
     LanguageClientOptions,
     ServerOptions,
-    TransportKind,
+    // TransportKind,
 } from 'vscode-languageclient/node';
 
 let client: LanguageClient;
 
-export function activate(context: ExtensionContext) {
-    // The server is implemented in node
-    const serverModule = context.asAbsolutePath(
-        path.join('server', 'out', 'server.js')
-    );
+export function activate(_context: ExtensionContext) {
+    // // The server is implemented in node
+    // const serverModule = context.asAbsolutePath(
+    //     path.join('server', 'out', 'server.js')
+    // );
 
-    // If the extension is launched in debug mode then the debug server options are used
-    // Otherwise the run options are used
-    const serverOptions: ServerOptions = {
-        run: { module: serverModule, transport: TransportKind.ipc },
-        debug: {
-            module: serverModule,
-            transport: TransportKind.ipc,
-        }
-    };
+    // // If the extension is launched in debug mode then the debug server options are used
+    // // Otherwise the run options are used
+    // const serverOptions: ServerOptions = {
+    //     run: { module: serverModule, transport: TransportKind.ipc },
+    //     debug: {
+    //         module: serverModule,
+    //         transport: TransportKind.ipc,
+    //     }
+    // };
 
     // Options to control the language client
     const clientOptions: LanguageClientOptions = {
         // Register the server for plain text documents
-        documentSelector: [{ scheme: 'file', language: 'plaintext' }],
+        documentSelector: [{ scheme: 'file', language: 'pdf' }],
         synchronize: {
             // Notify the server about file changes to '.clientrc files contained in the workspace
             fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
@@ -41,7 +41,7 @@ export function activate(context: ExtensionContext) {
     client = new LanguageClient(
         'languageServerExample',
         'Language Server Example',
-        serverOptions,
+        {} as ServerOptions, // serverOptions,
         clientOptions
     );
 
