8f5d58500a928ff617d4ef31e3201124bc4ca8a7
Victor Pogor <victor.pogor@outlook.com>
2025-03-18 23:18:20 +1000

    refactor

--- crates/parser/src/grammar/items.rs
+++ crates/parser/src/grammar/items.rs
@@ -7,37 +7,22 @@ pub(super) fn pdf_body(p: &mut Parser<'_>) {
     }
 }
 
-pub(super) fn pdf_item(p: &mut Parser<'_>) {
-    let m = p.start();
-
-    let m = match opt_item(p, m) {
-        Ok(()) => {
-            return;
-        }
-        Err(m) => m,
-    };
-
-    m.abandon(p);
-    atom::atom_expr(p);
-}
-
-/// Try to parse an item, completing `m` in case of success.
-pub(super) fn opt_item(p: &mut Parser<'_>, m: Marker) -> Result<(), Marker> {
+pub(super) fn pdf_item(p: &mut Parser<'_>) -> Option<CompletedMarker> {
     let la = p.nth(1);
     match p.current() {
-        INT_NUMBER if la == INT_NUMBER && p.nth_at_ts(2, INDIRECT_KEYWORDS) => indirect_reference(p, m),
-        _ => return Err(m),
+        INT_NUMBER if la == INT_NUMBER && p.nth_at_ts(2, INDIRECT_KEYWORDS) => indirect_obj_or_reference(p),
+        _ => atom::atom_expr(p),
     }
-    Ok(())
 }
 
-fn indirect_reference(p: &mut Parser<'_>, m: Marker) {
+fn indirect_obj_or_reference(p: &mut Parser<'_>) -> Option<CompletedMarker> {
     assert!(p.at(INT_NUMBER));
+    let m = p.start();
     atom::atom_expr(p); // object number
     assert!(p.at(INT_NUMBER));
     atom::atom_expr(p); // generation number
     assert!(p.at_ts(INDIRECT_KEYWORDS));
     let k = p.current();
     atom::atom_expr(p); // obj or R keyword
-    m.complete(p, if k == T![obj] { OBJECT_ID } else { INDIRECT_REFERENCE_EXPR });
+    Some(m.complete(p, if k == T![obj] { OBJECT_ID } else { INDIRECT_REFERENCE_EXPR }))
 }
