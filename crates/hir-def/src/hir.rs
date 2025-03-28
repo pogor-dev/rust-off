5d6212996343aa5bc851c5750401cb919e11b5eb
Victor Pogor <victor.pogor@outlook.com>
2025-03-29 07:50:44 +1000

    added hir-def

---
+++ crates/hir-def/src/hir.rs
@@ -0,0 +1,13 @@
+//! This module describes hir-level representation of expressions.
+//!
+//! This representation is:
+//!
+//! 1. Identity-based. Each expression has an `id`, so we can distinguish
+//!    between different `1` in `1 + 1`.
+//! 2. Independent of syntax. Though syntactic provenance information can be
+//!    attached separately via id-based side map.
+//! 3. Unresolved. Paths are stored as sequences of names, and not as defs the
+//!    names refer to.
+//! 4. Desugared. There's no `if let`.
+//!
+//! See also a neighboring `body` module.
--- crates/hir-def/src/lib.rs
+++ crates/hir-def/src/lib.rs
@@ -1,1 +1,10 @@
+//! `hir_def` crate contains everything between macro expansion and type
+//! inference.
+//!
+//! It defines various items (structs, enums, traits) which comprises Rust code,
+//! as well as an algorithm for resolving paths to such entities.
+//!
+//! Note that `hir_def` is a work in progress, so not all of the above is
+//! actually true.
 
+pub mod hir;
