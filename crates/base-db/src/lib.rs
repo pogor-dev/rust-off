9b856f3338fa5eadaaccfc4fce7a3b2cb933ab96
Victor Pogor <victor.pogor@outlook.com>
2025-04-11 21:04:58 +1000

    added db

--- crates/base-db/src/lib.rs
+++ crates/base-db/src/lib.rs
@@ -1,2 +1,46 @@
 //! base_db defines basic database traits. The concrete DB is defined by ide.
 // FIXME: Rename this crate, base db is non descriptive
+
+use std::hash::BuildHasherDefault;
+
+use dashmap::{DashMap, mapref::entry::Entry};
+use rustc_hash::FxHasher;
+use salsa::Setter;
+use triomphe::Arc;
+
+#[derive(Debug, Default)]
+pub struct Files {
+    files: Arc<DashMap<vfs::FileId, FileText, BuildHasherDefault<FxHasher>>>,
+}
+
+impl Files {
+    pub fn file_text(&self, file_id: vfs::FileId) -> FileText {
+        *self.files.get(&file_id).expect("Unable to fetch file; this is a bug")
+    }
+
+    pub fn set_file_text(&self, db: &mut dyn SourceDatabase, file_id: vfs::FileId, text: &[u8]) {
+        match self.files.entry(file_id) {
+            Entry::Occupied(mut occupied) => {
+                occupied.get_mut().set_text(db).to(Arc::from(text));
+            }
+            Entry::Vacant(vacant) => {
+                let text = FileText::new(db, Arc::from(text), file_id);
+                vacant.insert(text);
+            }
+        };
+    }
+}
+
+#[salsa::input]
+pub struct FileText {
+    pub text: Arc<[u8]>,
+    pub file_id: vfs::FileId,
+}
+
+#[salsa::db]
+pub trait SourceDatabase: salsa::Database {
+    /// Text of the file.
+    fn file_text(&self, file_id: vfs::FileId) -> FileText;
+
+    fn set_file_text(&mut self, file_id: vfs::FileId, text: &[u8]);
+}
